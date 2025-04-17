6f9da1d136eecb6a93294ee600ce4804e13efc53
Nahid Sheikh <mdnahidyn@gmail.com>
2025-04-17 14:31:17 +0600

    setup project and make it responsible

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+const Homepage = () => {
+    return (
+        <div>
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+            <p>
+                Aliquam sint praesentium, incidunt, laboriosam aperiam molestias asperiores, nostrum ex officia eligendi est. Voluptas tempora ab, adipisci ratione perferendis vitae dolor cumque. Ipsam aspernatur, consectetur omnis ullam temporibus commodi! Quis culpa dolorem, enim at ut porro placeat maxime ipsam eveniet, numquam provident?
+            </p>
+        </div>
+    )
+};
+
+export default Homepage;
