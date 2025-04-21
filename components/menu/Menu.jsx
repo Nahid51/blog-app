7e7649ab37e051bbc83afb2e8812832a5c60a3e4
Nahid Sheikh <mdnahidyn@gmail.com>
2025-04-21 14:58:25 +0600

    added responsive sidebar in homepage

--- components/menu/Menu.jsx
+++ components/menu/Menu.jsx
@@ -5,14 +5,14 @@ import styles from './menu.module.css';
 const Menu = () => {
     return (
         <div className={styles.container}>
-            <h2 className={styles.subtitle}>{"What's hot"}</h2>
-            <h1 className={styles.title}>Most Popular</h1>
+            <h3 className={styles.subtitle}>{"What's hot"}</h3>
+            <h2 className={styles.title}>Most Popular</h2>
             <MenuPosts withImage={false} />
-            <h2 className={styles.subtitle}>Discover by topic</h2>
-            <h1 className={styles.title}>Categories</h1>
+            <h3 className={styles.subtitle}>Discover by topic</h3>
+            <h2 className={styles.title}>Categories</h2>
             <MenuCategories />
-            <h2 className={styles.subtitle}>Chosen by the editor</h2>
-            <h1 className={styles.title}>Editors Pick</h1>
+            <h3 className={styles.subtitle}>Chosen by the editor</h3>
+            <h2 className={styles.title}>Editors Pick</h2>
             <MenuPosts withImage={true} />
         </div>
     )
--- components/menuPosts/MenuPosts.jsx
+++ components/menuPosts/MenuPosts.jsx
@@ -1,8 +1,83 @@
+import Link from "next/link";
+import Image from "next/image";
 import styles from "./menuPosts.module.css";
 
-const MenuPosts = () => {
+const MenuPosts = ({ withImage }) => {
     return (
-        <div>MenuPosts</div>
+        <div className={styles.items}>
+            <Link href="/" className={styles.item}>
+                {withImage && (
+                    <div className={styles.imgContainer}>
+                        <Image src="/p1.jpeg" alt="" fill />
+                    </div>
+                )}
+                <div className={styles.textContainer}>
+                    <span className={`${styles.category} ${styles.travel}`}>Travel</span>
+                    <h3 className={styles.postTitle}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </h3>
+                    <div className={styles.detail}>
+                        <span className={styles.username}>Kane William - </span>
+                        <span className={styles.date}>10.10.2024</span>
+                    </div>
+                </div>
+            </Link>
+            <Link href="/" className={styles.item}>
+                {withImage && (
+                    <div className={styles.imgContainer}>
+                        <Image src="/p1.jpeg" alt="" fill />
+                    </div>
+                )}
+                <div className={styles.textContainer}>
+                    <span className={`${styles.category} ${styles.culture}`}>
+                        Culture
+                    </span>
+                    <h3 className={styles.postTitle}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </h3>
+                    <div className={styles.detail}>
+                        <span className={styles.username}>Kane William - </span>
+                        <span className={styles.date}>10.10.2024</span>
+                    </div>
+                </div>
+            </Link>
+            <Link href="/" className={styles.item}>
+                {withImage && (
+                    <div className={styles.imgContainer}>
+                        <Image src="/p1.jpeg" alt="" fill />
+                    </div>
+                )}
+                <div className={styles.textContainer}>
+                    <span className={`${styles.category} ${styles.food}`}>Food</span>
+                    <h3 className={styles.postTitle}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </h3>
+                    <div className={styles.detail}>
+                        <span className={styles.username}>Kane William - </span>
+                        <span className={styles.date}>10.10.2024</span>
+                    </div>
+                </div>
+            </Link>
+            <Link href="/" className={styles.item}>
+                {withImage && (
+                    <div className={styles.imgContainer}>
+                        <Image src="/p1.jpeg" alt="" fill />
+                    </div>
+                )}
+                <div className={styles.textContainer}>
+                    <span className={`${styles.category} ${styles.fashion}`}>
+                        Fashion
+                    </span>
+                    <h3 className={styles.postTitle}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </h3>
+                    <div className={styles.detail}>
+                        <span className={styles.username}>Kane William - </span>
+                        <span className={styles.date}>10.10.2024</span>
+                    </div>
+                </div>
+            </Link>
+        </div>
     )
 }
 
